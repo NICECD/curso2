7e734bc553388b02767824720df410a3be69cfa9
Berenice Contreras Dominguez <bnicecd@gmail.com>
2018-03-17 11:16:49 -0600

    update

---
+++ app/Departamento.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Departamento extends Model
+{
+    protected $table ='cat_departamentos';
+   protected $fillable = ['codigo','descripcion'];
+
+   public function empleados()
+   {
+   	 return $this->hasMany('App\Empleado','id_departamento','id');
+   } //
+  /* public function setDescripcionAtribute($value)
+   {
+   	 $this->attributes['descripcion'] mb_strtoupper($value,'utf-8');
+   } //*/
+}
---
+++ database/migrations/2018_03_17_145543_create_table_turno.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableTurno extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cat_turnos',function(Blueprint $table){
+            $table->increments('id');
+            $table->string('descripcion');//250
+            $table->timestamps();
+        });
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cat_turnos');
+
+
+        //
+    }
+}
